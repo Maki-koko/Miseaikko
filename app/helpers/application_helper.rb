a0705efbe2a1503a67d3c6749e2b7fbd57012219
Maki-koko <144005937+Maki-koko@users.noreply.github.com>
2023-12-02 10:53:42 +0000

    admin修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,14 +8,26 @@ module ApplicationHelper
 
   def color_by_score(score)
     if score >= 0.25 && score <= 1.0
-      "#008000" #緑
+      "#008000" #緑　良い
     elsif score >= -0.25 && score <= 0.25
-      "#ffd700" #黄色
+      "#ffd700" #黄色　良くない
     elsif score >= -1.0 && score <= -0.25
-      "#b22222" #赤
+      "#b22222" #赤　悪い
     else
       "default"
     end
   end
 
+# text_by_score　数値の結果で表示する文字を変える
+  def text_by_score(score)
+    if score >= 0.25 && score <= 1.0
+      "良い" #良い
+    elsif score >= -0.25 && score <= 0.25
+      "普通" #普通
+    elsif score >= -1.0 && score <= -0.25
+      "悪い" #悪い
+    else
+      "-"
+    end
+  end
 end
